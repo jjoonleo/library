1edf238139ff2eae575028362d069cabdb5dc110
Ejun Park <jjoonleo@gmail.com>
2023-07-19 10:30:28 +0900

    try catch delete console.log("tryCatch")

--- server/utils/tryCatch.js
+++ server/utils/tryCatch.js
@@ -1,6 +1,6 @@
 exports.tryCatch = (controller) => async (req, res, next) => {
     try {
-        console.log("tryCatch");
+        //console.log("tryCatch");
         await controller(req, res);
     } catch (error) {
         console.log("error catched");
